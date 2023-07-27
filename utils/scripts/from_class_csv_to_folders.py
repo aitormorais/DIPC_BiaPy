2b0876b822e470018440a868bdfce183629b75ea
AITOR <aitormorais@opendeusto.es>
2023-07-27 15:10:42 +0200

    mañana_arreglar

--- utils/scripts/from_class_csv_to_folders.py
+++ utils/scripts/from_class_csv_to_folders.py
@@ -8,25 +8,18 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser(description="Organize classification data",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-
 parser.add_argument("-image_dir", "--image_dir", help="Image directory")
-parser.add_argument("-csv", "--csv", help="CSV file with the class of each image. Must contain three columns with no header.")
+parser.add_argument("-csv", "--csv", help="CSV file with the class of each image.")
 parser.add_argument("-out_dir", "--out_dir",help="Output directory to organize the images into")
-parser.add_argument("-BiaPy_dir", "--BiaPy_dir", help="BiaPy path")
 args = vars(parser.parse_args())
 
-sys.path.insert(0, args['BiaPy_dir'])
+df = pd.read_csv(args['csv'])
 
-df = pd.read_csv(args['csv'], header=None)
+if 'filename' not in df.columns or 'label' not in df.columns:
+    raise ValueError("CSV file need to contain at least two columns: 'filename' and 'label'")
 
-if len(df.columns) != 3:
-    raise ValueError("CSV file need to contain three columns with no header")
-
-df.columns = ["phase", "filename", "class"]
-
-df = df.reset_index()
 for index, row in tqdm(df.iterrows()):
-    dest_folder = os.path.join(args['out_dir'], str(row['phase']).lower(), str(row['class']))
+    dest_folder = os.path.join(args['out_dir'], str(row['label']).replace(" ", "_").lower())
     os.makedirs(dest_folder, exist_ok=True)
 
     filename = os.path.join(args['image_dir'], row['filename'])
